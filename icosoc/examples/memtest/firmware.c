df9a30c35a457a5201506f96877a76b89087e399
Clifford Wolf <clifford@clifford.at>
2016-06-13 11:47:35 +0200

    Added PMOD LED blinking tests to memtest example

--- icosoc/examples/memtest/firmware.c
+++ icosoc/examples/memtest/firmware.c
@@ -81,10 +81,27 @@ int main()
 
 	console_puts(found_error ? "ERROR\n" : "OK\n");
 
-	for (int k = 0;; k = ~k) {
+	uint32_t ledstrip = 0x11111111;
+	*(volatile uint32_t*)(0x20000004 + 1 * 0x10000) = ~0;
+	*(volatile uint32_t*)(0x20000000 + 1 * 0x10000) = ledstrip;
+
+	for (int k = 0;; k = (k+1) & 63)
+	{
 		for (int i = 0; i < 500000; i++)
 			asm volatile ("");
-		setled(k & (found_error ? 4 : 3));
+
+		setled((k & 1) ? (found_error ? 4 : 3) : 0);
+
+		if (k == 0)
+			ledstrip = 0x11111111;
+		else if (k < 16)
+			ledstrip = (ledstrip << 1) | (ledstrip >> 31);
+		else if (k < 24)
+			ledstrip = ~0;
+		else
+			ledstrip = (k & 1) ? ~0 : 0;
+
+		*(volatile uint32_t*)(0x20000000 + 1 * 0x10000) = ledstrip;
 	}
 }
 
