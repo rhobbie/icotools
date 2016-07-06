953197037c76c67e277d3a450d409b9a8dcb1c5a
Clifford Wolf <clifford@clifford.at>
2016-07-06 19:34:57 +0200

    Improved ledpanel example

--- icosoc/examples/ledpanel/main.c
+++ icosoc/examples/ledpanel/main.c
@@ -11,24 +11,35 @@ int main()
 	setbuf(stdout, NULL);
 	putchar(0);
 
-	// display sprite0 on first panel
-	for (int i = 0; i < 1024; i++)
-		videomem[1024+i] = sprite0[i];
-
-	// animate sprite1/sprite2 on second panel
-	while (1)
+	for (int k = 0;; k++)
 	{
+		volatile uint32_t *panel0 = videomem;
+		volatile uint32_t *panel1 = videomem;
+
+		if (k % 2 == 0)
+			panel0 += 1024;
+		else
+			panel1 += 1024;
+
+		// statically display sprite0 on one panel
 		for (int i = 0; i < 1024; i++)
-			videomem[i] = sprite1[i];
+			panel0[i] = sprite0[i];
 
-		for (int i = 0; i < 1000000; i++)
-			asm volatile ("");
+		// animate sprite1/sprite2 on other panel
+		for (int l = 0; l < 3; l++)
+		{
+			for (int i = 0; i < 1024; i++)
+				panel1[i] = sprite1[i];
 
-		for (int i = 0; i < 1024; i++)
-			videomem[i] = sprite2[i];
+			for (int i = 0; i < 1000000; i++)
+				asm volatile ("" ::: "memory");
 
-		for (int i = 0; i < 1000000; i++)
-			asm volatile ("");
+			for (int i = 0; i < 1024; i++)
+				panel1[i] = sprite2[i];
+
+			for (int i = 0; i < 1000000; i++)
+				asm volatile ("" ::: "memory");
+		}
 	}
 }
 
