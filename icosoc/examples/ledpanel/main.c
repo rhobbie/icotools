e2793b09956d7228f87dee8a6817939c688ba68d
Clifford Wolf <clifford@clifford.at>
2016-07-06 20:39:59 +0200

    Improved ledpanel example

--- icosoc/examples/ledpanel/main.c
+++ icosoc/examples/ledpanel/main.c
@@ -4,9 +4,28 @@
 #include "icosoc.h"
 #include "sprites.h"
 
+int ledstatus = 0;
+
+void irq_handler(uint32_t irq_mask, uint32_t *regs)
+{
+	if (irq_mask & 1) {
+		ledstatus = ledstatus >= 30 ? 1 : ledstatus+1;
+		if (ledstatus > 15)
+			icosoc_ledstrip_set(1 << (30-ledstatus));
+		else
+			icosoc_ledstrip_set(1 << ledstatus);
+		icosoc_timer(1000000);
+	}
+}
+
 int main()
 {
 	volatile uint32_t *videomem = (void*)(0x20000000 + 1 * 0x10000);
+
+	icosoc_ledstrip_dir(0xffff);
+	icosoc_irq(irq_handler);
+	icosoc_maskirq(0);
+	icosoc_timer(1000000);
 
 	setbuf(stdout, NULL);
 	putchar(0);
@@ -28,14 +47,22 @@ int main()
 		// animate sprite1/sprite2 on other panel
 		for (int l = 0; l < 3; l++)
 		{
+			icosoc_leds((1 << (k%2)) | 4);
+
 			for (int i = 0; i < 1024; i++)
 				panel1[i] = sprite1[i];
+
+			icosoc_leds(1 << (k%2));
 
 			for (int i = 0; i < 1000000; i++)
 				asm volatile ("" ::: "memory");
 
+			icosoc_leds((1 << (k%2)) | 4);
+
 			for (int i = 0; i < 1024; i++)
 				panel1[i] = sprite2[i];
+
+			icosoc_leds(1 << (k%2));
 
 			for (int i = 0; i < 1000000; i++)
 				asm volatile ("" ::: "memory");
