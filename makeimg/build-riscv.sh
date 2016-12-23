15045f1102961a8ffda4537b2fb6009260cea3f3
Clifford Wolf <clifford@clifford.at>
2016-12-23 19:36:29 +0000

    Improvements in makeimg scripts

--- makeimg/build-riscv.sh
+++ makeimg/build-riscv.sh
@@ -5,8 +5,10 @@ set -ex
 rm -rf /usr/local/src/picorv32
 git clone https://github.com/cliffordwolf/picorv32.git /usr/local/src/picorv32
 
+patch /usr/local/src/picorv32/Makefile < riscv-build-hack.diff
+cp riscv-sim-hack.diff /usr/local/src/picorv32/
 cd /usr/local/src/picorv32
-git checkout riscv-gnu-toolchain-update
+
 make download-tools
 
 rm -rf /opt/riscv32{i,ic,im,imc}
