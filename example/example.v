923365ede75721cbd85c46758f6de3e586376878
Clifford Wolf <clifford@clifford.at>
2017-01-07 16:12:15 +0000

    Added pmods to example

--- example/example.v
+++ example/example.v
@@ -1,4 +1,11 @@
-module top (input clk_100mhz, output reg led1, led2, led3);
+module top (
+	input clk_100mhz,
+	output reg led1, led2, led3,
+	output pmod1_1, pmod1_2, pmod1_3, pmod1_4, pmod1_7, pmod1_8, pmod1_9, pmod1_10,
+	output pmod2_1, pmod2_2, pmod2_3, pmod2_4, pmod2_7, pmod2_8, pmod2_9, pmod2_10,
+	output pmod3_1, pmod3_2, pmod3_3, pmod3_4, pmod3_7, pmod3_8, pmod3_9, pmod3_10,
+	output pmod4_1, pmod4_2, pmod4_3, pmod4_4, pmod4_7, pmod4_8, pmod4_9, pmod4_10
+);
 
 	// Clock Generator
 
@@ -71,4 +78,19 @@ module top (input clk_100mhz, output reg led1, led2, led3);
 		led3 <= (counter > counter_led3 + (1 << (COUNTER_BITS-1))) && !state_led3[2:0];
 	end
 
+	// Testing the PMODs
+
+	reg [21:0] pmod_counter = 0;
+	reg [7:0] pmod_leds = 0;
+
+	always @(posedge clk) begin
+		if (!pmod_counter)
+			pmod_leds <= &pmod_leds ? 0 : {pmod_leds, 1'b1};
+		pmod_counter <= pmod_counter + 1;
+	end
+
+	assign {pmod1_1, pmod1_2, pmod1_3, pmod1_4, pmod1_7, pmod1_8, pmod1_9, pmod1_10} = pmod_leds;
+	assign {pmod2_1, pmod2_2, pmod2_3, pmod2_4, pmod2_7, pmod2_8, pmod2_9, pmod2_10} = pmod_leds;
+	assign {pmod3_1, pmod3_2, pmod3_3, pmod3_4, pmod3_7, pmod3_8, pmod3_9, pmod3_10} = pmod_leds;
+	assign {pmod4_1, pmod4_2, pmod4_3, pmod4_4, pmod4_7, pmod4_8, pmod4_9, pmod4_10} = pmod_leds;
 endmodule
