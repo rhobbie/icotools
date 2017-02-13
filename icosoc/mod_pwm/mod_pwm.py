3f83fef76b6c9dbd0b8489f900b8e2bc5f812dff
Clifford Wolf <clifford@clifford.at>
2017-02-13 15:47:59 +0100

    Add missing volatile keywords to mod_pwm C code

--- icosoc/mod_pwm/mod_pwm.py
+++ icosoc/mod_pwm/mod_pwm.py
@@ -3,42 +3,42 @@ def generate_c_code(icosoc_h, icosoc_c, mod):
     code = """
 static inline void icosoc_@name@_setcounter(uint32_t val)
 {
-    *(uint32_t*)(0x20000000 + @addr@ * 0x10000) = val;
+    *(volatile uint32_t*)(0x20000000 + @addr@ * 0x10000) = val;
 }
 
 static inline void icosoc_@name@_setmaxcnt(uint32_t val)
 {
-    *(uint32_t*)(0x20000004 + @addr@ * 0x10000) = val;
+    *(volatile uint32_t*)(0x20000004 + @addr@ * 0x10000) = val;
 }
 
 static inline void icosoc_@name@_setoncnt(uint32_t val)
 {
-    *(uint32_t*)(0x20000008 + @addr@ * 0x10000) = val;
+    *(volatile uint32_t*)(0x20000008 + @addr@ * 0x10000) = val;
 }
 
 static inline void icosoc_@name@_setoffcnt(uint32_t val)
 {
-    *(uint32_t*)(0x2000000c + @addr@ * 0x10000) = val;
+    *(volatile uint32_t*)(0x2000000c + @addr@ * 0x10000) = val;
 }
 
 static inline uint32_t icosoc_@name@_getcounter()
 {
-    return *(uint32_t*)(0x20000000 + @addr@ * 0x10000);
+    return *(volatile uint32_t*)(0x20000000 + @addr@ * 0x10000);
 }
 
 static inline uint32_t icosoc_@name@_getmaxcnt()
 {
-    return *(uint32_t*)(0x20000004 + @addr@ * 0x10000);
+    return *(volatile uint32_t*)(0x20000004 + @addr@ * 0x10000);
 }
 
 static inline uint32_t icosoc_@name@_getoncnt()
 {
-    return *(uint32_t*)(0x20000008 + @addr@ * 0x10000);
+    return *(volatile uint32_t*)(0x20000008 + @addr@ * 0x10000);
 }
 
 static inline uint32_t icosoc_@name@_getoffcnt()
 {
-    return *(uint32_t*)(0x2000000c + @addr@ * 0x10000);
+    return *(volatile uint32_t*)(0x2000000c + @addr@ * 0x10000);
 }
 """
     code = code.replace("@name@", mod["name"])
